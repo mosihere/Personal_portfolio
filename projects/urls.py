4c86ec250c7296c9ca1cbd52bf6c0f408b7b8686
Mosihere <mostafakhoshhal@yahoo.com>
2021-01-19 16:16:52 +0330

    A simple Blog is ready

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_index, name ='project_index'),
-    path('<int:pk>/', views.project_detail, name ='project_detail'),
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
+# URL Path name will use for project_index.html when we want to
+# 
